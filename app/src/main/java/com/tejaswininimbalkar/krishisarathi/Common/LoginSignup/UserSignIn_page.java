1d2e7e2bae256bb4dcf2915b1834f863bf84114f
Jayesh <jayeshborase82@gmail.com>
2021-03-19 16:59:38 +0530

    New branch "Jayesh_code" Login code

---
+++ app/src/main/java/com/tejaswininimbalkar/krishisarathi/Common/LoginSignup/UserSignIn_page.java
@@ -0,0 +1,16 @@
+package com.tejaswininimbalkar.krishisarathi.Common.LoginSignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tejaswininimbalkar.krishisarathi.R;
+
+public class UserSignIn_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_sign_in_page);
+    }
+}
