a1a52ca9768690ae296fc2a4c0d63b1052b86465
Tejaswini <tejaswininimbalkar2000@gmail.com>
2021-03-31 17:43:49 +0530

    Edit profile picture

--- app/src/main/java/com/tejaswininimbalkar/krishisarathi/EditProfileActivity.java
+++ app/src/main/java/com/tejaswininimbalkar/krishisarathi/EditProfileActivity.java
@@ -1,8 +1,10 @@
 package com.tejaswininimbalkar.krishisarathi;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class EditProfileActivity extends AppCompatActivity {
@@ -19,5 +21,20 @@ public class EditProfileActivity extends AppCompatActivity {
 
     public void cancelEdit(View view) {
         onBackPressed();
+    }
+
+    public void selectPicture(View view) {
+        pictureResourceDialog();
+    }
+
+    private void pictureResourceDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.profile_picture_source, null);
+        dialog.setCancelable(false);
+        dialog.setView(dialogView);
+
+        AlertDialog alertDialog = dialog.create();
+        alertDialog.show();
     }
 }
