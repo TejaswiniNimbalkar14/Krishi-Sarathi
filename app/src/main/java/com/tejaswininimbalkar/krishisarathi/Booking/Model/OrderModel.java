d238928459625f03ddb6218bc8e2ae9a7bd1ab42
d-spy <62275413+d-spy@users.noreply.github.com>
2021-04-26 13:15:20 +0530

    This commit from Devendra

---
+++ app/src/main/java/com/tejaswininimbalkar/krishisarathi/Booking/Model/OrderModel.java
@@ -0,0 +1,46 @@
+package com.tejaswininimbalkar.krishisarathi.Booking.Model;
+
+public class OrderModel {
+
+    String pname,paddress,pphone;
+    String order_name;
+
+    public OrderModel(String order_name, String pname, String paddress, String pphone) {
+        this.order_name = order_name;
+        this.pname = pname;
+        this.paddress = paddress;
+        this.pphone = pphone;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public String getPaddress() {
+        return paddress;
+    }
+
+    public void setPaddress(String paddress) {
+        this.paddress = paddress;
+    }
+
+    public String getPphone() {
+        return pphone;
+    }
+
+    public void setPphone(String pphone) {
+        this.pphone = pphone;
+    }
+
+    public String getOrder_name() {
+        return order_name;
+    }
+
+    public void setOrder_name(String order_name) {
+        this.order_name = order_name;
+    }
+}
