ebb51ae2bf4efb24c652aba24027dd990cefa970
d-spy <62275413+d-spy@users.noreply.github.com>
2021-05-03 18:20:36 +0530

    first commit of Booking 1

--- app/src/main/java/com/tejaswininimbalkar/krishisarathi/Booking/BookingActivity.java
+++ app/src/main/java/com/tejaswininimbalkar/krishisarathi/Booking/BookingActivity.java
@@ -7,10 +7,14 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.tejaswininimbalkar.krishisarathi.Booking.Model.OrderModel;
 import com.tejaswininimbalkar.krishisarathi.R;
 
@@ -18,8 +22,10 @@ public class BookingActivity extends AppCompatActivity {
 
 
     TextView name, address, phone;
-    TextView order_name;
+    TextView order_name,owner_userID;
     Button btn;
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    DatabaseReference root = db.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +40,24 @@ public class BookingActivity extends AppCompatActivity {
         name = findViewById(R.id.pname);
         address = findViewById(R.id.paddress);
         phone = findViewById(R.id.pphone);
-
+        owner_userID = findViewById(R.id.owner_userID);
 
         btn = findViewById(R.id.book);
+
+        root.child(s).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String ownerID = snapshot.child("userName").getValue().toString();
+                owner_userID.setText(ownerID);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+        });
 
 
     }
